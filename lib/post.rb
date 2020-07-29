2046e8334bba7cd19d4025df8180c3fa2b0c813d
Katie Le <lethikatie@gmail.com>
2020-07-29 18:42:10 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+  attr_accessor :author, :title, :author_name
+  @@all = []
+  def initialize(title)
+    @title = title
+    @@all << self 
+end 
+
+  def author_name
+    self.author.name
+  end
+
+ def self.all 
+    @@all 
+  end 
+  
+end

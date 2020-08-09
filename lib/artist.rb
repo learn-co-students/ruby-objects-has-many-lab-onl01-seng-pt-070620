c475021d4885692760300515bc11234dd5d0109a
Lauren Stone <lstone321@gmail.com>
2020-08-09 19:52:37 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+class Artist  
+  attr_accessor :name, :songs 
+  
+  @@song_count = 1 
+
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end 
+  
+  
+  def add_song(song)
+    @songs << song 
+    song.artist= self 
+    @@song_count += 1 
+  end 
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def Artist.song_count
+    @@song_count
+  end 
+  
+end 

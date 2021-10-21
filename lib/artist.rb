38c4759e983c410f1c01b23e263c903dd162b826
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-10-21 06:14:16 -0400

    rewrite

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,27 @@
 class Artist
-  
+
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
-    @songs = []
   end
   
   def songs
-    Song.all.select {|song| song.artist == self}
+    Song.all.select { |song| song.artist == self }
   end
-  
+
   def add_song(song)
-    @songs << song
     song.artist = self
   end
-  
-  def add_song_by_name(name)
-    song = Song.new(name)
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
     add_song(song)
   end
-  
+
   def self.song_count
-    Song.all.count
+    Song.all.count { |song| song.artist != nil }
   end
-  
+
 end
+
